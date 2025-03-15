0e44e9bf92e1281901fc69daa70273c2cafcc2d6
wggc <3409885610@qq.com>
2025-03-15 17:11:00 +0800

    Update build.sh to package specific files for MySQL, Dnf_Gate, and Game
    This commit modifies the build.sh script to create tar.gz archives containing only the necessary files for each component. The MySQL archive now includes init_sql, my.cnf, and the MySQL RPMs. The Game archive is updated to include relevant directories and scripts, while the Dnf_Gate archive is enhanced with additional configuration and server files. This change improves the packaging process and ensures that only essential files are included in the archives.

--- build.sh
+++ build.sh
@@ -2,8 +2,33 @@
 
 find . -name ".DS_Store" -type f -delete
 
-cd ./MySQL ; tar -czvf MySQL.tar.gz ./root ; mv MySQL.tar.gz ../ ; cd ../
+cd ./MySQL
+tar -czvf MySQL.tar.gz \
+    ./init_sql \
+    ./my.cnf \
+    ./MySQL-client-5.5.62-1.el6.x86_64.rpm \
+    ./MySQL-server-5.5.62-1.el6.x86_64.rpm
+mv MySQL.tar.gz ../
+cd ../
 
-cd ./Dnf_Gate ; tar -czvf Dnf_Gate.tar.gz ./home ./root ./usr ; mv Dnf_Gate.tar.gz ../ ; cd ../
+cd ./Game
+tar -czvf Game.tar.gz \
+    ./dp2 \
+    ./home \
+    ./usr \
+    ./run \
+    ./stop
+mv Game.tar.gz ../
+cd ../
 
-cd ./Game ; tar -czvf Game.tar.gz ./dp2 ./home ./root ./usr ; mv Game.tar.gz ../ ; cd ../
+cd ./Dnf_Gate
+tar -czvf Dnf_Gate.tar.gz \
+    ./home \
+    ./usr \
+    ./Config.ini \
+    ./DnfGateServer \
+    ./GateRestart \
+    ./GateStop \
+    ./privatekey.pem
+mv Dnf_Gate.tar.gz ../
+cd ../

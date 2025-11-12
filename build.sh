39a8ccc8f8749d1112c6a0e56ae4c8645b5c0111
wggc <3409885610@qq.com>
2025-11-12 11:11:52 +0800

    添加构建脚本

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "开始打包安装文件: Game.tar.gz、MySQL.tar.gz"
+
+rm -rf Game.tar.gz MySQL.tar.gz
+
+COPYFILE_DISABLE=1 tar -cvzf Game.tar.gz Game
+COPYFILE_DISABLE=1 tar -cvzf MySQL.tar.gz MySQL
+
+echo "打包完成: Game.tar.gz、MySQL.tar.gz"

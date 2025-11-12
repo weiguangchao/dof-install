3f9713db3437098818ccf5aec693604350500254
wggc <3409885610@qq.com>
2025-11-12 14:50:03 +0800

    更新`build.sh`

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ echo "开始打包安装文件: Game.tar.gz、MySQL.tar.gz"
 
 rm -rf Game.tar.gz MySQL.tar.gz
 
-COPYFILE_DISABLE=1 tar -cvzf Game.tar.gz Game
-COPYFILE_DISABLE=1 tar -cvzf MySQL.tar.gz MySQL
+COPYFILE_DISABLE=1 tar -cvzf Game.tar.gz -C Game . 
+
+COPYFILE_DISABLE=1 tar -cvzf MySQL.tar.gz -C MySQL . 
 
 echo "打包完成: Game.tar.gz、MySQL.tar.gz"

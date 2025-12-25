6a3cb03ca743b1e7dbfed59ed5ec7472bc9edddc
wggc <3409885610@qq.com>
2025-12-25 14:29:25 +0800

    chore: update dof.sh script for installation adjustments

--- dof.sh
+++ dof.sh
@@ -115,7 +115,7 @@ function install_yum_dependency() {
 
     mv /etc/yum.repos.d /etc/yum.repos.d.bak
     mkdir /etc/yum.repos.d
-    curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
+    curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.cloud.tencent.com/repo/centos7_base.repo
 
     yum clean all
     yum makecache

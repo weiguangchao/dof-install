0809cd035a666c9c867afd52d726144ddcd85aef
weiguangchao <3409885610@qq.com>
2026-04-18 07:45:38 +0800

    refactor(dof.sh): grant execute permissions to GameRestart script
    - Add command to grant executable permissions to the GameRestart script, ensuring it can be executed without permission issues.
    - This change enhances the usability of the game server installation process by preventing potential execution errors.

--- dof.sh
+++ dof.sh
@@ -378,6 +378,7 @@ function install_game_server() {
     # 给 run 和 stop 脚本授予可执行权限
     chmod +x ./run
     chmod +x ./stop
+    chmod +x ./GameRestart
 
     log_success "Game Server安装完成, 服务器IP: $server_ip"
 }
